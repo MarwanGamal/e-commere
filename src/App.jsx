2c0f7b2ac8730674276fb07d66791e50710bd4f7
MarwanGamal <marawangamal000@gmail.com>
2023-03-29 07:20:09 +0200

    changed browser router to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Product from "./pages/Product"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Cart from "./pages/Cart"
-import{createBrowserRouter, RouterProvider} from 'react-router-dom'
+import{createBrowserRouter, RouterProvider,createHashRouter} from 'react-router-dom'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element:<Home />,
