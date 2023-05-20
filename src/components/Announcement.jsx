89105d1dbc6241891810568ca52679e7af21ac50
MarwanGamal <marawangamal000@gmail.com>
2023-05-20 05:02:48 +0300

    Changed the font size of the announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -8,7 +8,7 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
     font-weight: 500;
-    font-size: 14px;
+    font-size: 18px;
 `
 
 export default function Announcement() {
